297adc74df05dcbe2874d6596a1b7099f4f7db43
Alemancenter <info@alemancenter.com>
2026-05-30 16:35:05 +0200

    sdf

--- src/app/actions/revalidate-settings.ts
+++ src/app/actions/revalidate-settings.ts
@@ -3,5 +3,5 @@
 import { revalidateTag } from 'next/cache';
 
 export async function revalidateFrontSettings(): Promise<void> {
-  revalidateTag('front-settings');
+  revalidateTag('front-settings', 'max');
 }
